050bc90e67bd50439e547d7a71d5530265b5ffcc
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-10-15 21:15:54 +0530

    Added apiKeys

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,1 +1,14 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={08b8dfdef863b1d3dc42087532e6b63e}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}
+// https://api.openweathermap.org/data/2.5/weather?q={kanpur}&appid={08b8dfdef863b1d3dc42087532e6b63e}
+
+const apiKey = "08b8dfdef863b1d3dc42087532e6b63e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore;";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + '&appid=${apiKey}');
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
