a47c2f94b71f1579e3ab0643bb2ac4e6ab882fd9
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-10-16 21:12:54 +0530

    Added API working

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,14 +1,22 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}
-// https://api.openweathermap.org/data/2.5/weather?q={kanpur}&appid={08b8dfdef863b1d3dc42087532e6b63e}
-
 const apiKey = "08b8dfdef863b1d3dc42087532e6b63e";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore;";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+// const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + '&appid=${apiKey}');
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp+" °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed+" km/h";
 }
 
-checkWeather();
+
+// searchBtn.addEventListener("click", ()=>{
+//     checkWeather(searchBox.value);
+// })
+
