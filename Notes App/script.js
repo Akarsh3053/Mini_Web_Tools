d01dacc9100b1eb7f9664eecfee74bfd0723ebcc
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-10-23 21:28:41 +0530

    Added storage methods

--- Notes App/script.js
+++ Notes App/script.js
@@ -2,6 +2,16 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -14,5 +24,20 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
   }
 });
