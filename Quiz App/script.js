a6bcc7901493a6514de66de4d763cda84ac6703a
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-12-07 22:34:13 +0530

    added quiz funtions

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -50,4 +50,27 @@ const questionElement = document.getElementById("question");
 const answerButton = document.querySelector(".answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-let curretQuestionIndex = 0;
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestions();
+}
+
+function showQuestions() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestion + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
+
+startQuiz();
